19eb2f6477a6718357434bebfbe4f26082f82920
tanaka_kei <tk1209adgjmptw@gmail.com>
2019-10-28 22:18:43 +0900

    blog公開用

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { Insomnia } from '@ionic-native/insomnia/ngx';//追加
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,6 +16,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    Insomnia,//追加
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {Insomnia} from '@ionic-native/insomnia/ngx'
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +7,20 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private insomnia:Insomnia) {}
+  ionViewDidEnter(){
+    this.insomnia.keepAwake()
+    .then(
+      ()=>{
+        console.log("success");
+      }
+    ).catch(e=>{
+        console.log("error",e);
+    })
+  }
 
+
+  sleepAgain(){
+    this.insomnia.allowSleepAgain();
+  }
 }
